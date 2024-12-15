83f0796532acf3b66309a20f98588e06127924e5
Melonps <kake.kake0311@gmail.com>
2024-12-16 00:40:39 +0900

    [FIX] language detectionを追加

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,7 @@
 import { Container, Box, Text } from '@yamada-ui/react'
 import { useEffect, useState } from 'react'
 import { GatheredWordsTable } from './components/GatherdWordTable'
+import guesslanguage from 'guesslanguage'
 
 function App(): JSX.Element {
   const [clipboardText, setClipboardText] = useState('test')
@@ -11,12 +12,18 @@ function App(): JSX.Element {
     })
   }, [])
 
+  console.log(
+    guesslanguage.guessLanguage.name('吃饱了然后睡着了刚刚', (callback) => {
+      console.log(callback)
+    })
+  )
   return (
     <Container>
       <Box border="1px solid" rounded="md" borderColor="border">
         <Text>クリップボードの内容</Text>
         <Text>吃饱了然后睡着了刚刚</Text>
         <Text>{clipboardText}</Text>
+        <Text>言語判定</Text>
         <GatheredWordsTable />
       </Box>
     </Container>
